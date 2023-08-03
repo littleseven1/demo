97d7a67a780a1aef487d0dc549f5b8321744c936
dqy <2658298553@qq.com>
2023-08-03 16:45:43 +0800

    qq

--- src/main/java/com/example/project/mapper/ActionPieMapper.java
+++ src/main/java/com/example/project/mapper/ActionPieMapper.java
@@ -6,27 +6,27 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface ActionPieMapper {
-    @Select("SELECT COUNT(*) FROM action_${fileKey}")
+    @Select("SELECT user_id, COUNT(*) as count FROM action_${fileKey} GROUP BY user_id")
     int getTotalRecordCount(@Param("fileKey") String fileKey);
 
-    @Select("SELECT COUNT(*) as count FROM action_${fileKey} WHERE num > 1000")
+    @Select("SELECT user_id, COUNT(*) as count FROM action_${fileKey} WHERE num > 1000 GROUP BY user_id")
     int countCategory1(@Param("fileKey") String fileKey);
 
-    @Select("SELECT COUNT(*) as count FROM action_${fileKey} WHERE num BETWEEN 700 AND 1000")
+    @Select("SELECT user_id, COUNT(*) as count FROM action_${fileKey} WHERE num BETWEEN 700 AND 1000 GROUP BY user_id ")
     int countCategory2(@Param("fileKey") String fileKey);
 
-    @Select("SELECT COUNT(*) as count FROM action_${fileKey} WHERE num BETWEEN 500 AND 700")
+    @Select("SELECT user_id, COUNT(*) as count FROM action_${fileKey} WHERE num BETWEEN 500 AND 700 GROUP BY user_id")
     int countCategory3(@Param("fileKey") String fileKey);
 
-    @Select("SELECT COUNT(*) as count FROM action_${fileKey} WHERE num BETWEEN 300 AND 500")
+    @Select("SELECT user_id, COUNT(*) as count FROM action_${fileKey} WHERE num BETWEEN 300 AND 500 GROUP BY user_id")
     int countCategory4(@Param("fileKey") String fileKey);
 
-    @Select("SELECT COUNT(*) as count FROM action_${fileKey} WHERE num BETWEEN 200 AND 300")
+    @Select("SELECT user_id, COUNT(*) as count FROM action_${fileKey} WHERE num BETWEEN 200 AND 300 GROUP BY user_id")
     int countCategory5(@Param("fileKey") String fileKey);
 
-    @Select("SELECT COUNT(*) as count FROM action_${fileKey} WHERE num BETWEEN 100 AND 200")
+    @Select("SELECT user_id, COUNT(*) as count FROM action_${fileKey} WHERE num BETWEEN 100 AND 200 GROUP BY user_id")
     int countCategory6(@Param("fileKey") String fileKey);
 
-    @Select("SELECT COUNT(*) as count FROM action_${fileKey} WHERE num < 100")
+    @Select("SELECT user_id, COUNT(*) as count FROM action_${fileKey} WHERE num < 100 GROUP BY user_id")
     int countCategory7(@Param("fileKey") String fileKey);
 }
